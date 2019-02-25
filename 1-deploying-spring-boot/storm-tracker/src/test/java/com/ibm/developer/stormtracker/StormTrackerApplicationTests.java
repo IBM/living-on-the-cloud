666abb59cbcf7498e5fe2869e367e406783c2da5
Billy Korando <wkorando@gmail.com>
2019-02-25 08:52:12 -0600

    initial import for living on the cloud code

---
+++ 1-deploying-spring-boot/storm-tracker/src/test/java/com/ibm/developer/stormtracker/StormTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibm.developer.stormtracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StormTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
