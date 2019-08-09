b2e598ec4947f0fc40cd26e222002ea0c0263a53
Billy Korando <wkorando@gmail.com>
2019-08-09 13:51:27 -0500

    Clean up

--- 4-connecting-to-a-kafka-service/storm-tracker/README.md
+++
@@ -1,3 +0,0 @@
-# Deploying Spring Boot to Kubernetes
-
-This a demonstration project on how to deploy a Spring Boot application to a Kubernetes cluster running on IBM Cloud. 
--- 4-connecting-to-a-kafka-service/storm-tracker/src/test/resources/init-db.sql
+++
@@ -1,4 +0,0 @@
-create sequence storms_id_generator start 10 increment 1;
-create table storms (id int8 not null, end_date varchar(255), end_location varchar(255), intensity int4, start_date varchar(255), start_location varchar(255), type varchar(255), primary key (id));
-INSERT INTO storms (id, start_date, end_date, start_location, end_location, type, intensity) VALUES (nextval('storms_id_generator'), '10-10-2018', '10-13-2018', 'Denver, Colorado', 'Kansas City, Missouri', 'Thunderstorm', 2);
-INSERT INTO storms (id, start_date, end_date, start_location, end_location, type, intensity) VALUES (nextval('storms_id_generator'), '01-05-2019', '01-17-2019', 'Pacific Ocean', 'New York City, New Yor', 'Blizzard', 4);
