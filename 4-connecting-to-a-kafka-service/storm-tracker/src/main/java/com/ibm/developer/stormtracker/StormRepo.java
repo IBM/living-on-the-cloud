2536d2e6711f872c3353266596c96ef1adad4695
Billy Korando <wkorando@gmail.com>
2019-08-09 12:19:59 -0500

    restructuring project repo

--- 4-connecting-to-a-kafka-service/storm-tracker/src/main/java/com/ibm/developer/stormtracker/StormRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ibm.developer.stormtracker;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StormRepo extends CrudRepository<Storm, Long> {
-	public Iterable<Storm> findByType(String type);
-}
